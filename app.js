ca592985013262b1a5aee0753d0a73ce236b8eec
KenanMehmet <kenmehmet1@gmail.com>
2023-04-11 15:08:45 +0100

    added basic button and updating variable

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 let clicks = 0;
 let clickers_level_one = 0;
 let clickers_level_two = 0;
+document.querySelector('#click-amount').innerHTML = clicks
 
-clickAdd = (clicks) => {
+clickAdd = () => {
     clicks++;
+    document.querySelector('#click-amount').innerHTML = clicks
 }
+
 
 
